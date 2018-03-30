10f238e498b270a3181daf4f8cf33889882442d1
Ignacio Cruz <igmoweb@gmail.com>
2018-03-30 13:34:51 +0200

    Firs version

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -172,7 +172,7 @@ function archive() {
   var time = dateFormat(new Date(), "yyyy-mm-dd_HH-MM");
   var pkg = JSON.parse(fs.readFileSync('./package.json'));
   var title = pkg.name + '_' + time + '.zip';
-
+console.log( PATHS.package );
   return gulp.src(PATHS.package)
     .pipe($.zip(title))
     .pipe(gulp.dest('packaged'));
