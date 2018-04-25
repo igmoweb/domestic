f12e6984abacc77b6b3839203f5b8f81cde52c48
Ignacio Cruz <igmoweb@gmail.com>
2018-04-25 12:32:08 +0200

    Added taxonomy description to archive pages

--- archive.php
+++ archive.php
@@ -2,6 +2,8 @@
 
 	<main id="main" class="columns <?php echo esc_attr( domestic_get_content_class( 'main' ) ); ?>">
 		<?php the_archive_title( '<h1 class="page-title">', '</h1>' ) ?>
+		<?php the_archive_description( '<div class="taxonomy-description">', '</div>' ); ?>
+
 		<?php get_template_part( 'template-parts/loop' ); ?>
 	</main>
 
